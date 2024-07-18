16d0ea0a98ab114e2d72b6a98fb85994d41b3688
ranga <srirangaemani@gmail.com>
2024-07-18 11:42:36 -0400

    -cleanup and documentation

--- backend/mealfinder/admin.py
+++ backend/mealfinder/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Meal
 
 class MealAdmin(admin.ModelAdmin):
-    list_display = ('dish', 'thumbnail')# Register your models here.
+    list_display = ('dish', 'thumbnail')
 
 admin.site.register(Meal, MealAdmin)
--- backend/mealfinder/views.py
+++ backend/mealfinder/views.py
@@ -5,16 +5,17 @@ from .models import Meal
 from django.http import JsonResponse
 import requests
 
-# Create your views here.
-
 def search_meals(request):
+    ## request debugging
     print(request.GET)
+    ## parse api query keys
     ingredient = request.GET.get('ingredient')
     cuisine = request.GET.get('cuisine')
-
+    ## error checking
     if (not ingredient or ingredient.strip() == "") and (not cuisine or cuisine.strip() == ""):
         return JsonResponse({'error': 'No ingredient or cuisine provided'}, status=400)
 
+    ## conditional api url setup
     if ingredient and ingredient.strip() != "":
         url = f"https://www.themealdb.com/api/json/v1/1/filter.php?i={ingredient}"
     else:
@@ -22,16 +23,17 @@ def search_meals(request):
 
     response = requests.get(url)
 
+    ## response population
     if response.status_code == 200:
         data = response.json()
         meals = data.get('meals')
-
         response = JsonResponse({'meals': meals})
         response['access-control-allow-origin'] = 'http://localhost:8000'
         return response
-
+    ## error handling
     return JsonResponse({'error': 'API request failed'}, status=500)
 
+## serializing meal data using predefined object schema
 class MealView(viewsets.ModelViewSet):
     serializer_class = MealSerializer
     queryset = Meal.objects.all()
